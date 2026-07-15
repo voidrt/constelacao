61d1d317cb55c7c09b14920e16fab549518c3e98
voidrt <edbracaioli@gmail.com>
2026-07-15 01:07:43 -0300

    ...

--- constants.hpp
+++ constants.hpp
@@ -1,4 +1,12 @@
 #pragma once
 
-inline constexpr float kWindowHeight = 1000;
-inline constexpr float kWindowWidth = 744;
+inline constexpr float kWindowHeight = 600;
+inline constexpr float kWindowWidth = 544;
+inline constexpr int kParticleCount = 500;
+inline constexpr float kParticleSpeed = 17.5;
+inline constexpr float kDeltaTime = 0.016;
+inline constexpr float kLineRadius = 625;
+inline constexpr float kCentripetalForce = 3.1f;
+inline constexpr float kBHoleRadius = 15.0f;
+inline constexpr float kStarSize = 2.0f;
+
--- main.cpp
+++ main.cpp
@@ -1,22 +1,71 @@
 #include <iostream>
 #include "raylib.h"
+#include <raymath.h>
 #include <climits>
 #include "constants.hpp"
-#include <iostream>
 #include <array>
+
+float GetRandomFValue(const float min, const float max)
+{
+    return min + ((max - min) * static_cast<float>((GetRandomValue(1, INT_MAX)) / static_cast<float>(INT_MAX))); // NOLINT(*-narrowing-conversions)
+}
+
+Vector2 VectorMod(const Vector2 vec, const Vector2 vec2)
+{
+    const Vector2 mod = {vec.x - (vec2.x * floorf(vec.x / vec2.x)), vec.y - (vec2.y * floorf(vec.y / vec2.y))};
+    return mod;
+}
+
+float GetRandomFNormalized()
+{
+    const float random = GetRandomFValue(-1, 1);
+    return random / std::fabsf(random);
+}
 
 struct Particle
 {
     Vector2 position;
     Vector2 velocity;
+    int id;
+
+    void Move()
+    {
+        constexpr Vector2 center = {kWindowWidth / 2.0f, kWindowHeight / 2.0f};
+        const Vector2 centripetal = center - this->position;
+        const float distanceToCenterSqr = Vector2DistanceSqr(center, this->position);
+
+        if (distanceToCenterSqr > (5.0f))
+        {
+            this->velocity += (Vector2Normalize(centripetal) * kCentripetalForce) / (distanceToCenterSqr + kBHoleRadius);
+        }
+
+        this->position += (this->velocity * kDeltaTime);
+
+        this->position = VectorMod(this->position, {kWindowWidth, kWindowHeight});
+    }
+
+    void MakeConstellation(const std::array<Particle, kParticleCount>& particles) const
+    {
+        for (size_t i{}; i < kParticleCount; ++i)
+        {
+            if (this->id == i)
+            {
+                continue;
+            }
+
+            Particle otherParticle = particles[i];
+            float distanceSqr = Vector2DistanceSqr(this->position, otherParticle.position);
+
+            if (distanceSqr < kLineRadius)
+            {
+                DrawLineEx(this->position, otherParticle.position, 1, RAYWHITE);
+            }
+        }
+    }
 };
 
-Particle particles[1000];
+std::array<Particle, kParticleCount> particles;
 
-float GetRandomFValue(float min, float max)
-{
-    return min + ((max - min) * static_cast<float>((GetRandomValue(1, INT_MAX)) / static_cast<float>(INT_MAX)));
-}
 
 int InitParticles()
 {
@@ -25,40 +74,65 @@ int InitParticles()
     float velocityX{};
     float velocityY{};
 
-    for (size_t i{}; i < 1000; ++i)
+    for (size_t i{}; i < kParticleCount; ++i)
     {
         positionX = (GetRandomFValue(0, kWindowWidth));
         positionY = (GetRandomFValue(0, kWindowHeight));
-        particles[i].position = {positionX,positionY};
+
+        velocityX = GetRandomFValue(-kParticleSpeed, kParticleSpeed);
+        velocityY = GetRandomFValue(-kParticleSpeed, kParticleSpeed);
+
+        particles[i].position = {positionX, positionY};
+        particles[i].velocity = {velocityX, velocityY};
+        particles[i].id = static_cast<int>(i);
     }
 
     return 0;
 }
 
+void RunSimulation()
+{
+    for (size_t i{}; i < kParticleCount; ++i)
+    {
+        particles[i].MakeConstellation(particles);
+    }
+
+    if (IsKeyPressed(KEY_R))
+    {
+        InitParticles();
+    }
+
+    for (size_t i{}; i < kParticleCount; ++i)
+    {
+        particles[i].Move();
+    }
+}
+
 int main()
 {
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(kWindowWidth, kWindowHeight, "Constellations");
-
+    SetTargetFPS(60);
     InitParticles();
 
-    Camera2D camera = { 0 };
-    camera.target = {kWindowWidth/2.0f,kWindowHeight/2.0f};
-
-    camera.offset = {kWindowWidth/2.0f,kWindowHeight/2.0f};
-    camera.zoom=.8f;
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        BeginMode2D(camera);
-        for (const Particle particle : particles)
+        DrawFPS(15, 15);
+
+        DrawCircleV({kWindowWidth / 2.0f, kWindowHeight / 2.0f}, kBHoleRadius, {51, 51, 37, 255});
+
+        for (const auto particle : particles)
         {
-            DrawPixelV(particle.position, RAYWHITE);
+            Rectangle star = {particle.position.x, particle.position.y, kStarSize, kStarSize};
+            DrawRectanglePro(star, {kStarSize / 2.0f,kStarSize / 2.0f}, atan2f(particle.velocity.y, particle.velocity.x) * RAD2DEG, RAYWHITE);
         }
+        RunSimulation();
+
         EndDrawing();
-        EndMode2D();
     }
 
     CloseWindow();
